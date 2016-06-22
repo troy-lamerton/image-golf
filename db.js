c1d1e7d048baadcc21501f32c8a6e808cf6404c5
rich-farr <scarletmanuka@gmail.com>
2016-06-23 11:14:37 +1200

    Add tape & tap-spec & test object

---
+++ db.js
@@ -0,0 +1,9 @@
+{
+	"images": [
+	{"id": 1, "image": "http://www.abc.net.au/news/image/6473316-3x2-940x627.jpg"},
+	{"id": 2, "image": "https://managewp.com/wp-content/uploads/2012/07/spam.jpg"},
+	{"id": 3, "image": "http://www.motherearthnews.com/~/media/Images/MEN/Editorial/Articles/Magazine%20Articles/2006/04-01/Backyard%20Fish%20Farming/Tilapia.jpgg"},
+	{"id": 4, "image": "http://www.overbitespictures.com/wp-content/uploads/2012/09/CS_9.3.12_labor_day_OPP.jpg"},
+	{"id": 5, "image": "https://i.ytimg.com/vi/IDEaqFo7-a8/hqdefault.jpg"},
+	]
+}
